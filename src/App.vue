502134e83192d48910c798163846e1f9b292c2ed
ChrisOspina <ChrisOspina99@gmail.com>
2025-07-27 15:18:27 -0400

    lesson 4

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script setup>
-import { ref } from 'vue'
-import ProductDisplay from '@/components/ProductDisplay.vue'
+ import { ref } from 'vue';
+ import socksGreenImage from './assets/images/socks_green.jpeg'
 
-const cart = ref([])
-const premium = ref(true)
+ const product = ref('Socks');
+ const image = ref(socksGreenImage)
+ const inventory = ref(100)
+ const onSale = ref(TextTrackCue)
 
-const updateCart = (id) => {
-  cart.value.push(id)
-}
+ const description = ref('Just some new socks on sale');
 </script>
-  
+
 <template>
-  <div class="nav-bar"></div>
-  <div class="cart">Cart({{ cart.length }})</div>
-  <ProductDisplay :premium="premium" @add-to-cart="updateCart"></ProductDisplay>
+<div class="nav-bar"></div>
+<div class="product-display">
+  <div class="product-container">
+    <div class="product-image">
+      <img v-bind:src="image"/>
+    </div>
+    <div class="product-info">
+      <h1>{{ product }}</h1>
+      <p v-if="inventory > 10">In stock</p>
+      <p v-else-if="inventory <=10  && inventory > 0">Almost sold out</p>
+      <p v-else>Out of stock</p>
+      <br/>
+      <p v-if="onSale"><strong>On Sale</strong></p>
+    </div>
+  </div>
+</div>
 </template>
