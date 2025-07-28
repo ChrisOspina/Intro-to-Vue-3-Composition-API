520da143b23513792b32c0d992b5ba41894ac8f8
ChrisOspina <ChrisOspina99@gmail.com>
2025-07-27 21:41:36 -0400

    Lesson 9

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,16 @@
 <script setup>
- import { ref, computed } from 'vue';
- import socksGreenImage from './assets/images/socks_green.jpeg';
- import socksBlueImage from './assets/images/socks_blue.jpeg';
+ import {ref} from 'vue';
+ import ProductDisplay from './components/ProductDisplay.vue';
 
-
- const product = ref('Socks');
- const brand = ref('Vue Mastery')
-
- const selectedVariant = ref(0)
-
- const details = ref(['50% cotton', '30% wool', '20% polyester'])
- const variants = ref([
-  {id: 2224, color:'green', image: socksGreenImage, quantity: 50},
-  {id: 2225, color:'blue', image: socksBlueImage, quantity: 0},
-])
-
-const cart = ref(0)
-
-const title = computed(() =>{
-  return brand.value +  ' ' + product.value
-})
-
-const addToCart = () => cart.value +=1;
-
-const updateVariant = (index)=>{
-  selectedVariant.value = index
-  console.log(index)
-}
-
-const image = computed(()=>{
-  return variants.value[selectedVariant.value].image
-})
-
-const inStock = computed(()=>{
-  return variants.value[selectedVariant.value].quantity > 0
-})
+  const cart = ref(0)
+  const premium = ref(false)
 
 </script>
 
 <template>
-<div class="nav-bar"></div>
-<div class="cart">Cart({{ cart }})</div>
-<div class="product-display">
-  <div class="product-container">
-    <div class="product-image">
-      <img v-bind:src="image"/>
-    </div>
-    <div class="product-info">
-      <h1>{{ title}}</h1>
-      <p v-if="inStock">In stock</p>
-      <p v-else>Out of stock</p>
-      <ul>
-        <li v-for="detail in details">{{ detail }}</li>
-      </ul>
-      <div 
-        v-for="(variant, index) in variants" 
-        :key="variant.id"
-        @mouseover="updateVariant(index)"
-        class="color-circle"
-        :style="{backgroundColor: variant.color}"
-      >
-      </div>
-      <button 
-        class="button" 
-        :class="{ disabledButton: !inStock }"
-        v-on:click="addToCart"
-        :disabled="!inStock"
-      >
-      Add to Cart
-    </button>
-    </div>
-  </div>
-</div>
+  <div class="nav-bar"></div>
+  <div class="cart">Cart({{ cart }})</div>
+  <ProductDisplay :premium="premium"/>
+  <ProductDisplay/>
+  <ProductDisplay/>
 </template>
